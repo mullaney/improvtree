fd82ecab35e70bccc25ffb7b2db986530bc9b42e
Collin Miller (Zardulist) <collintmiller@gmail.com>
2016-07-27 21:54:56 -0500

    allow for ratings

--- app/controllers/student/reps_controller.rb
+++ app/controllers/student/reps_controller.rb
@@ -2,6 +2,6 @@ class Student::RepsController < Student::ResourceController
   def resource_class; Rep end
 
   def authorize_update!
-    @resource.from_node === current_student or raise User::NotAuthorized
+    @resource.enrollment.student === current_student or raise User::NotAuthorized
   end
 end
